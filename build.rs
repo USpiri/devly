938d1c4d792a495de548075873fb72adbd7436d6
Uriel Spiridione <urielspiridione135@gmail.com>
2023-03-23 00:15:02 -0300

    📦 Build: Added function to copy devly_commits.toml to release folder

--- build.rs
+++ build.rs
@@ -2,5 +2,5 @@ use std::fs;
 
 fn main() {
     fs::copy("devly_commits.toml", "target/debug/devly_commits.toml").unwrap();
-    // fs::copy("devly_commits.toml", "target/release/devly_commits.toml").unwrap();
+    fs::copy("devly_commits.toml", "target/release/devly_commits.toml").unwrap();
 }
