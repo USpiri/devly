9af3ee0cea05061e9a29306b29043c73d2c3abdf
Uriel Spiridione <urielspiridione135@gmail.com>
2023-03-22 22:05:26 -0300

    ✨ Feat: Add custom build to use devly_commits.toml file

---
+++ build.rs
@@ -0,0 +1,6 @@
+use std::fs;
+
+fn main() {
+    fs::copy("devly_commits.toml", "target/debug/devly_commits.toml").unwrap();
+    // fs::copy("devly_commits.toml", "target/release/devly_commits.toml").unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
+mod args;
+mod commit_types;
+
 use args::Commands;
 use clap::Parser;
+use commit_types::load_commit_types;
 
 use crate::args::Cli;
-
-mod args;
 
 fn main() {
     let cli:Cli = Cli::parse();
     match &cli.command {
         Some(Commands::Commit {}) => {
-            println!("Devly commit command");
+            let commit_types = load_commit_types();
+            match commit_types {
+                Ok(commit_types) => for commit_type in commit_types {
+                    println!("{commit_type}");
+                },
+                Err(e) =>  println!("ERROR: {}", e),
+            }
         },
         None => ()
     }
