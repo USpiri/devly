fcd02dd89ee807547c3bf2dc0bf0d05c97add9d4
Uriel Spiridione <urielspiridione135@gmail.com>
2023-03-25 17:46:49 -0300

    ✨ Feat: Reading file from installation path and current path

--- src/config.rs
+++ src/config.rs
@@ -9,14 +9,28 @@ pub struct Config {
 
 pub fn load_config() -> Result<Config, Box<dyn std::error::Error>> {
     // Instalation path
-    let mut current_exe = env::current_exe().expect("No se puede obtener la ruta actual");
-    current_exe.pop();
-    current_exe.push("devly_commits.toml");
+    let mut installation_path = env::current_exe().expect("Unable to get current path");
+    installation_path.pop();
+    installation_path.push("devly.toml");
 
-    // Reading devly_commits.toml file
-    let content = fs::read_to_string(current_exe)?;
+    // Reading devly.toml file
+    let mut config = load_config_from_file(&installation_path.to_str().unwrap())?;
+
+    // Reading current config file
+    if let Ok(current_dir_config) = load_config_from_file("devly.toml") {
+        config = Config {
+            ..current_dir_config
+        }
+    }
+
+    Ok(config)
+}
+
+fn load_config_from_file(path: &str) -> Result<Config, Box<dyn std::error::Error>> {
+
+    let content = fs::read_to_string(path)?;
     let data: Value = content.parse()?;
-    let config_value = data.get("Config").ok_or("Missing [Config] section")?;
+    let config_value = data.get("Config").ok_or(format!("Missing [Config] section on: {}", {path}))?;
     let config = Config::deserialize(config_value.clone())?;
 
     Ok(config)
